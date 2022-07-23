422eaf25bd7e39e3924c8b27f3079df607cbc11c
sungyong-lim <dlatjddyd524@gmail.com>
2022-07-23 23:30:35 +0900

    Create math.py

---
+++ Standard_Library/Math/math.py
@@ -0,0 +1,16 @@
+import math 
+
+# factorial 
+print(math.factorial(5)) # 5 팩토리얼
+
+# sqrt
+print(math.sqrt(7)) # 7의 제곱근 
+
+# gcd
+print(math.gcd(21, 14)) # 최대공약수  
+
+# pi
+print(math.pi) # PI
+
+# e 
+print(math.e) # 자연상수 e 
